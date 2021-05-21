cd7a7d6bd099f9c5eead681772907819d75dd8a0
bigfoxxx <ph.cool97@gmail.com>
2021-05-21 10:39:55 +0200

    Added android project

---
+++ application/app/src/main/java/com/holzhausen/notetaker/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.holzhausen.notetaker.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.holzhausen.notetaker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
